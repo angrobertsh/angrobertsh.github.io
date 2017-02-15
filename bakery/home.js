75647419da54e22dc01746eed0967a38fbf96431
angrobertsh <angrobertsh@gmail.com>
2017-02-15 01:35:24 -0800

    extendable
    yayay

--- bakery/home.js
+++ bakery/home.js
@@ -1,39 +1,39 @@
 var bakeryitems = [];
-var applenut = {name: "Apple Walnut Muffin", attributes: ["wg", "nuts", "fruit"], categories: {type: "Muffins", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old"}}
-var bananabread = {name: "Banana Bread Muffin", attributes: ["wg", "nuts", "fruit"], categories: {type: "Muffins", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old"}}
-var blueberry = {name: "Blueberry Muffin", attributes: ["wf", "fruit"], categories: {type: "Muffins", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old"}}
-var bran = {name: "Bran Muffin", attributes: ["wg", "fruit"], categories: {type: "Muffins", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old"}}
-var multi = {name: "Multigrain Muffin", attributes: ["wg", "nuts", "fruit"], categories: {type: "Muffins", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old"}}
-var cheddar = {name: "Cheddar Scone", attributes: [], categories: {type: "Scones", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old"}}
-var currant = {name: "Currant Scone", attributes: ["fruit"], categories: {type: "Scones", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old"}}
-var strawberry = {name:"Strawberry Scone", attributes: ["fruit"], categories: {type: "Scones", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old"}}
-var chocchip = {name:"Chocolate Chip Cookie", attributes: ["wg", "choc"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old"}}
-var doublechoc = {name:"Double Chocolate Cookie", attributes: ["vegan", "choc"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old"}}
-var oatmeal = {name:"Oatmeal Cookie", attributes: ["wg"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old"}}
-var pistachio = {name:"Pistachio Cookie", attributes: ["wf"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old"}}
-var snicker = {name:"Snickerdoodle Cookie", attributes: ["wg"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old"}}
-var thumb = {name:"Thumbprint Cookie", attributes: ["fruit", "nuts"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old"}}
-var walnuttea = {name:"Walnut Tea Cookie", attributes: ["nuts"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old"}}
-var applepie = {name:"Apple Pie", attributes: ["wg", "fruit"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old"}}
-var pecanpie = {name:"Pecan Pie", attributes: ["wg", "nuts"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old"}}
-var fruittart = {name:"Fruit Tart", attributes: ["wg", "fruit"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old"}}
-var lemontart = {name:"Lemon Tart", attributes: ["wg", "fruit"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old"}}
-var fruitcheese = {name:"Fruit Cheesecake", attributes: ["fruit"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old"}}
-var choccheese = {name:"Chocolate Cheesecake", attributes: ["choc"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old"}}
-var vancup = {name:"Vanilla Cupcake", attributes: ["fruit", "wf"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "Tossed", added: "Old"}}
-var raspcup = {name:"Raspberry Cupcake", attributes: ["fruit"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old"}}
-var germchoc = {name:"German Chocolate Cupcake", attributes: ["choc"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old"}}
-var carmchoc = {name:"Caramel Chocolate Cupcake", attributes: ["caramel"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old"}}
-var flourless = {name:"Flourless Chocolate Cake", attributes: ["choc", "wf"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old"}}
-var vegancake = {name:"Vegan Chocolate Cake", attributes: ["choc", "vegan"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old"}}
-var creampuff = {name:"Cream Puff", attributes: [], categories: {type: "Puffs-Eclairs", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old"}}
-var powderpuff = {name:"Powdered Sugar Cream Puff", attributes: [], categories: {type: "Puffs-Eclairs", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old"}}
-var chocpuff = {name:"Chocolate Drizzled Cream Puff", attributes: ["choc", "caramel"], categories: {type: "Puffs-Eclairs", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old"}}
-var carmeclair = {name:"Caramel Eclair", attributes: ["caramel"], categories: {type: "Puffs-Eclairs", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old"}}
-var choceclair = {name:"Chocolate Eclair", attributes: ["choc"], categories: {type: "Puffs-Eclairs", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old"}}
-var carmpud = {name:"Caramel Pudding", attributes: ["caramel"], categories: {type: "Miscellaneous", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old"}}
-var ricepud = {name:"Rice Pudding", attributes: ["wf"], categories: {type: "Miscellaneous", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old"}}
-var nutbar = {name:"Seeds and Nut Bar", attributes: ["raw", "vegan", "wf", "nuts"], categories: {type: "Miscellaneous", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old"}}
+var applenut = {name: "Apple Walnut Muffin", attributes: ["wg", "nuts", "fruit"], categories: {type: "Muffins", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old", perception: "Healthy"}}
+var bananabread = {name: "Banana Bread Muffin", attributes: ["wg", "nuts", "fruit"], categories: {type: "Muffins", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old", perception: "Healthy"}}
+var blueberry = {name: "Blueberry Muffin", attributes: ["wf", "fruit"], categories: {type: "Muffins", family: "Boulangerie", time: "All-Day", case: "Warm", sale: "Packaged", added: "Old", perception: "Classic"}}
+var bran = {name: "Bran Muffin", attributes: ["wg", "fruit"], categories: {type: "Muffins", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old", perception: "Healthy"}}
+var multi = {name: "Multigrain Muffin", attributes: ["wg", "nuts", "fruit"], categories: {type: "Muffins", family: "Boulangerie", time: "Morning", case: "Warm", sale: "Packaged", added: "Old", perception: "Healthy"}}
+var cheddar = {name: "Cheddar Scone", attributes: [], categories: {type: "Scones", family: "Boulangerie", time: "All-Day", case: "Warm", sale: "Packaged", added: "Old", perception: "Classic"}}
+var currant = {name: "Currant Scone", attributes: ["fruit"], categories: {type: "Scones", family: "Boulangerie", time: "All-Day", case: "Warm", sale: "Packaged", added: "Old", perception: "Classic"}}
+var strawberry = {name:"Strawberry Scone", attributes: ["fruit"], categories: {type: "Scones", family: "Boulangerie", time: "All-Day", case: "Warm", sale: "Packaged", added: "Old", perception: "Indulgent"}}
+var chocchip = {name:"Chocolate Chip Cookie", attributes: ["wg", "choc"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old", perception: "Classic"}}
+var doublechoc = {name:"Double Chocolate Cookie", attributes: ["vegan", "choc"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old", perception: "Indulgent"}}
+var oatmeal = {name:"Oatmeal Cookie", attributes: ["wg"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old", perception: "Classic"}}
+var pistachio = {name:"Pistachio Cookie", attributes: ["wf"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old", perception: "Healthy"}}
+var snicker = {name:"Snickerdoodle Cookie", attributes: ["wg"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old", perception: "Classic"}}
+var thumb = {name:"Thumbprint Cookie", attributes: ["fruit", "nuts"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old", perception: "Classic"}}
+var walnuttea = {name:"Walnut Tea Cookie", attributes: ["nuts"], categories: {type: "Cookies", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old", perception: "Classic"}}
+var applepie = {name:"Apple Pie", attributes: ["wg", "fruit"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old", perception: "Classic"}}
+var pecanpie = {name:"Pecan Pie", attributes: ["wg", "nuts"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old", perception: "Classic"}}
+var fruittart = {name:"Fruit Tart", attributes: ["wg", "fruit"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old", perception: "Indulgent"}}
+var lemontart = {name:"Lemon Tart", attributes: ["wg", "fruit"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old", perception: "Indulgent"}}
+var fruitcheese = {name:"Fruit Cheesecake", attributes: ["fruit"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old", perception: "Indulgent"}}
+var choccheese = {name:"Chocolate Cheesecake", attributes: ["choc"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old", perception: "Indulgent"}}
+var vancup = {name:"Vanilla Cupcake", attributes: ["fruit", "wf"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "Tossed", added: "Old", perception: "Indulgent"}}
+var raspcup = {name:"Raspberry Cupcake", attributes: ["fruit"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old", perception: "Indulgent"}}
+var germchoc = {name:"German Chocolate Cupcake", attributes: ["choc"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Warm", sale: "No", added: "Old", perception: "Indulgent"}}
+var carmchoc = {name:"Caramel Chocolate Cupcake", attributes: ["caramel"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old", perception: "Indulgent"}}
+var flourless = {name:"Flourless Chocolate Cake", attributes: ["choc", "wf"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old", perception: "Indulgent"}}
+var vegancake = {name:"Vegan Chocolate Cake", attributes: ["choc", "vegan"], categories: {type: "Cakes-Pies-Cupcakes", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old", perception: "Indulgent"}}
+var creampuff = {name:"Cream Puff", attributes: [], categories: {type: "Puffs-Eclairs", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old", perception: "Indulgent"}}
+var powderpuff = {name:"Powdered Sugar Cream Puff", attributes: [], categories: {type: "Puffs-Eclairs", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old", perception: "Indulgent"}}
+var chocpuff = {name:"Chocolate Drizzled Cream Puff", attributes: ["choc", "caramel"], categories: {type: "Puffs-Eclairs", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old", perception: "Indulgent"}}
+var carmeclair = {name:"Caramel Eclair", attributes: ["caramel"], categories: {type: "Puffs-Eclairs", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old", perception: "Indulgent"}}
+var choceclair = {name:"Chocolate Eclair", attributes: ["choc"], categories: {type: "Puffs-Eclairs", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "Tossed", added: "Old", perception: "Indulgent"}}
+var carmpud = {name:"Caramel Pudding", attributes: ["caramel"], categories: {type: "Miscellaneous", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old", perception: "Indulgent"}}
+var ricepud = {name:"Rice Pudding", attributes: ["wf"], categories: {type: "Miscellaneous", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old", perception: "Classic"}}
+var nutbar = {name:"Seeds and Nut Bar", attributes: ["raw", "vegan", "wf", "nuts"], categories: {type: "Miscellaneous", family: "Patisserie", time: "Afternoon", case: "Cold", sale: "No", added: "Old", perception: "Healthy"}}
 
 
 bakeryitems = [applenut, bananabread, blueberry, bran, multi, cheddar, currant, strawberry, chocchip, doublechoc, oatmeal, pistachio, snicker, thumb, walnuttea, applepie, pecanpie, fruittart, lemontart, fruitcheese, choccheese, vancup, raspcup, germchoc, carmchoc, flourless, vegancake, carmeclair, choceclair, chocpuff, powderpuff, creampuff, ricepud, carmpud, nutbar]
@@ -65,7 +65,7 @@ function populateInitialList()
 
 function populateListFromString()
 {
-	bakeryitems = JSON.parse(document.getElementByid("bakeryjson").value);
+	bakeryitems = JSON.parse(document.getElementById("bakeryjson").value);
 	createCategories();
 	setItems();
 }
@@ -75,18 +75,18 @@ function createCategories()
 	var helperhash = {};
 	let thecurrentsort = $('input[name=thesort]:checked', '#filteritems').val();
 	let currentUL;
+	let type;
 	bakeryitems.forEach(function(item){
-		item["categories"][thecurrentsort].forEach(function(type)){
-			if(helperhash[type] === undefined){
-				helperhash[type] = 1;
-				currentUL = document.createElement('ul');
-				currentUL.id = type;
-				currentUL.className = "categ";
-				currentUL.innerHTML = type;
-				document.getElementById("categlist").appendChild(currentUL);
-			} else {
-				helperhash[type] += 1;
-			}
+		type = item["categories"][thecurrentsort]
+		if(helperhash[type] === undefined){
+			helperhash[type] = 1;
+			currentUL = document.createElement('ul');
+			currentUL.id = type;
+			currentUL.className = "categ";
+			currentUL.innerHTML = type;
+			document.getElementById("categlist").appendChild(currentUL);
+		} else {
+			helperhash[type] += 1;
 		}
 	});
 }
@@ -96,7 +96,7 @@ function setItems()
 	let currentLI;
 	let thecurrentsort = $('input[name=thesort]:checked', '#filteritems').val();
 	let filternames = [];
-	let checker = []
+	let checker = [];
 	$('input[name=thefilter]:checked').each(function()
 		{
 				filternames.push($(this).val());
@@ -107,19 +107,17 @@ function setItems()
 			return item["attributes"].includes(filter);
 		}).includes(false) === false) {
 			currentLI = document.createElement('li');
-			currentLI.className = "categitem";
+			currentLI.className = "bakedgood";
 			currentLI.innerHTML = item["name"];
-			document.getElementById(thecurrentsort).appendChild(currentLI);
+			document.getElementById(item["categories"][thecurrentsort]).appendChild(currentLI);
 		}
 	});
-
-
 }
 
 $(document).ready(function(){
 
 
-	// populateInitialList();
+	populateInitialList();
 
 
 //	$('#toggleingredients').click(function()
@@ -128,61 +126,61 @@ $(document).ready(function(){
 //	});
 
 
-	$("#filteritems").submit(function()
+	$("#filteritems").submit(function(e)
 	{
-
+		e.preventDefault();
 		$("#categlist").empty();
 
-		//the sort
-		var thecurrentsort = $('input[name=thesort]:checked', '#filteritems').val();
+		populateListFromString();
 
-		//the filters
-		var filternames = [];
-		$('input[name=thefilter]:checked').each
-			(function()
-			{
-    			filternames.push($(this).val());
-    	});
+// 		//the sort
+// 		var thecurrentsort = $('input[name=thesort]:checked', '#filteritems').val();
+//
+// 		//the filters
+// 		var filternames = [];
+// 		$('input[name=thefilter]:checked').each
+// 			(function()
+// 			{
+//     			filternames.push($(this).val());
+//     	});
+//
+//
+// 		$.each(bakeryitems, function(index, value){
+// 			var categoryname = value["categories"][thecurrentsort];
+// //			check to see if div id doesn't exist by seeing if length is zero
+// 			if($("#" + categoryname).length == 0)
+// 			{
+// 				$("#categlist").append("<div class='categtitle' id='" + categoryname + "'><br>" + categoryname + "<br></div>");
+// //				create new header div attached to categlist with categoryname as id and text
+// 			}
+// 			if(filternames.length !== 0)
+// 			{
+// 				var counter = 0;
+// 				$.each(value["attributes"], function(index2, value2){
+// //					see if quality exists in filternames, index = -1 if it doesn't exist
+// 					if(jQuery.inArray(value2, filternames) !==-1)
+// 					{
+// 						counter++;
+// 					}
+// 				});
+// 				if(counter == filternames.length)
+// 				{
+// //						add array element 0 to the end of the div with id category name
+// 					$("#" + categoryname).append("<div class='bakedgood'>" + value["name"] + "</div>");
+// 				}
+// 			}
+// 			if(filternames.length == 0)
+// 			{
+// 				$("#" + categoryname).append("<div class='bakedgood'>" + value["name"] + "</div>");
+// 			}
+//
+// 		});
+//
+//
+//
+//
 
-
-		$.each(bakeryitems, function(index, value){
-			var categoryname = value["categories"][thecurrentsort];
-//			check to see if div id doesn't exist by seeing if length is zero
-			if($("#" + categoryname).length == 0)
-			{
-				$("#categlist").append("<div class='categtitle' id='" + categoryname + "'><br>" + categoryname + "<br></div>");
-//				create new header div attached to categlist with categoryname as id and text
-			}
-			if(filternames.length !== 0)
-			{
-				var counter = 0;
-				$.each(value["attributes"], function(index2, value2){
-//					see if quality exists in filternames, index = -1 if it doesn't exist
-					if(jQuery.inArray(value2, filternames) !==-1)
-					{
-						counter++;
-					}
-				});
-				if(counter == filternames.length)
-				{
-//						add array element 0 to the end of the div with id category name
-					$("#" + categoryname).append("<div class='bakedgood'>" + value["name"] + "</div>");
-				}
-			}
-			if(filternames.length == 0)
-			{
-				$("#" + categoryname).append("<div class='bakedgood'>" + value["name"] + "</div>");
-			}
-
-		});
-
-
-
-
-
-
-
-		return false;
+		// return false;
 	});
 
 });
